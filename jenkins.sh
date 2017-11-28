f4b9cee59f9944fc43950c843e586c5e89ca4a03
Kilaru <venkatakilaru10@gmail.com>
2017-11-28 16:40:25 -0500

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+apt-get install curl -y
+apt-get install git -y
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
+apt-get install jenkins
+netstat -tulpn | grep 8080
+systemctl jenkins start
+systemctl jenkins enable
