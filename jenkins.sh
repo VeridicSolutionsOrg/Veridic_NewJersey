ca92591b525d5e1644365e153a50e1fc2ef76d90
Kilaru <venkatakilaru10@gmail.com>
2017-11-28 16:48:15 -0500

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -5,6 +5,5 @@ wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key 
 sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-get update
 apt-get install jenkins
-netstat -tulpn | grep 8080
 systemctl jenkins start
 systemctl jenkins enable
