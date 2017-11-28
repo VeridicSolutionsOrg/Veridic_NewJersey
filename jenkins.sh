f4cbfab3cfbbb0bccc17c6957b748af700867795
Kilaru <venkatakilaru10@gmail.com>
2017-11-28 17:28:11 -0500

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
-apt-get install curl -y
-apt-get install git -y
+#Install Java
+sudo apt-get update
+sudo apt-get install default-jre
+sudo apt-get install default-jdk
+#Install Curl and Git
+sudo apt-get install curl
+sudo apt-get install git
+#Install Jenkins
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-apt-get update
-apt-get install jenkins
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins -y
+netstat -tulpn | grep 8080
 systemctl jenkins start
 systemctl jenkins enable
