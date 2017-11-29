e7f1dc2ceb89e067fadc48ca529b961c81a4de08
Kilaru <venkatakilaru10@gmail.com>
2017-11-29 12:22:49 -0500

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -8,9 +8,8 @@ sudo apt-get install curl
 sudo apt-get install git
 #Install Jenkins
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list' -y
 sudo apt-get update
 sudo apt-get install jenkins -y
 netstat -tulpn | grep 8080
-netstat -tulpn | grep 9090
 sudo /etc/init.d/jenkins start
